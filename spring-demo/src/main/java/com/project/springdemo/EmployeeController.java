61e788620c308094cf5f9b25e994f9dcad9c531f
Сергей <legenda.serzh@mail.ru>
2024-08-15 19:26:57 +0300

    sda

---
+++ spring-demo/src/main/java/com/project/springdemo/EmployeeController.java
@@ -0,0 +1,4 @@
+package com.project.springdemo;
+
+public class EmployeeController {
+}
---
+++ spring-demo/src/main/java/com/project/springdemo/EmployeeService.java
@@ -0,0 +1,13 @@
+package com.project.springdemo;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeService {
+    public static final int COUNT_EMPLOYEE = 3;
+    List<Employee> employees = new ArrayList<>(COUNT_EMPLOYEE);
+    new Employee("Петров", "Евгений")
+}
+
+
+
+
